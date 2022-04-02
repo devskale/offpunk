66454dc2e7eaaeaedc993c1e15c8e707fbf1f907
Lionel Dricot <git@ploum.eu>
2022-04-02 16:56:03 +0200

    releasing 1.3

--- offpunk.py
+++ offpunk.py
@@ -12,7 +12,7 @@
 #  - Björn Wärmedal <bjorn.warmedal@gmail.com>
 #  - <jake@rmgr.dev>
 
-_VERSION = "1.2"
+_VERSION = "1.3"
 global BETA
 BETA = False
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='offpunk',
-    version='1.2',
+    version='1.3',
     description="Offline-First Gemini/Web/Gopher/RSS reader and browser",
     author="Ploum",
     author_email="offpunk@ploum.eu",
