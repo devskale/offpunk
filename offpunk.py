24fe364f51d4aecfd97c2332d6583dd7c3352616
Ploum <git@ploum.eu>
2023-11-08 18:18:33 +0100

    returns the good version number

--- offpunk.py
+++ offpunk.py
@@ -4,7 +4,7 @@
 Offline-First Gemini/Web/Gopher/RSS reader and browser
 """
 
-__version__ = "2.0-beta1"
+__version__ = "2.0-beta2"
 
 ## Initial imports and conditional imports {{{
 import argparse
