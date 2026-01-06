42e29b9d18128806fd5ed255b8261e77e88d1747
Ploum <git@ploum.eu>
2026-01-06 21:27:32 +0100

    - Ansicat: HTML Render img with data-src if src doesn’t link to a valid image

--- ansicat.py
+++ ansicat.py
@@ -1395,7 +1395,6 @@ class HtmlRenderer(AbstractRenderer):
             elif element.name == "img":
                 src = element.get("src")
                 text = ""
-                ansi_img = render_image(src, width=width, mode=mode)
                 alt = element.get("alt")
                 if alt:
                     alt = sanitize_string(alt)
@@ -1408,7 +1407,12 @@ class HtmlRenderer(AbstractRenderer):
                     abs_url, data = looks_like_base64(src, self.get_base_url())
                     # if abs_url is None, it means we don’t support
                     # the image (such as svg+xml). So we hide it.
+                    # But we first check if there’s a data-src
+                    if not abs_url:
+                        src = element.get("data-src")
+                        abs_url, data = looks_like_base64(src,self.get_base_url())
                     if abs_url:
+                        ansi_img = render_image(src, width=width, mode=mode)
                         abs_url = abs_url.replace(" ","%20")
                         links.append(abs_url + " " + text)
                         self.images[mode].append(abs_url)
